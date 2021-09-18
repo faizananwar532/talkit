9452d6c7a2be4c240036c02261b58edb0738c50f
Awais <awaisshafiq503@gmail.com>
2021-09-18 14:42:00 +0000

    testing

--- backend/notification/app.js
+++ backend/notification/app.js
@@ -16,9 +16,20 @@ app.get('/', async (req, res, next) => {
 
 }, sendResponse);
 
-app.listen(process.env.PORT || 5000, () => {
-	
+app.listen(process.env.PORT || 5000, async () => {
+
 	console.log('App started at port.', process.env.PORT || 5000);
 	initChannelsSubscription([process.env.USER_CREATED_CHANNEL]);
+
+	// const res = await fetch('http://auth:4000/auth/verify/', {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzE5NzcwNTksInVzZXIiOnsidXNlcl9pZCI6MjIsInVzZXJuYW1lIjoiYXdhaXNfc2hhZmlxIiwiZW1haWwiOiJhd2Fpc3NoYWZpcTUwM0BnbWFpbC5jb20ifSwiaWF0IjoxNjMxOTczNDU5fQ.kn2YC7W5HnMqmjB4PTs0SI8z5FzWCF7ozBtpARuyecg'
+	// 	},
+	// });	
+
+	// const json = await res.json();
+
+	// console.log(json);
 
 });
