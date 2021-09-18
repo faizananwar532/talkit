fece1e4ac4fa794574e0859b5a7cbb2c7fdacbb8
AmmarSaqib <ammarsaqib123@gmail.com>
2021-09-18 15:42:45 +0500

    added something related to consumption

--- backend/chat/app/init_consumer.py
+++ backend/chat/app/init_consumer.py
@@ -3,31 +3,36 @@ Author: Ammar Saqib
 """
 
 import asyncio
+
 import aioredis
+import async_timeout
+from controllers.chat_controller import ChatController
+
+from database import get_db
 
 
 def start():
     """
     Starts the consumption logic from the auth app
     """
-
+    _db = get_db()
+    pub_sub = _db.pubsub()
 
     async def reader(channel: aioredis.client.PubSub):
         while True:
-            async with async_timeout.timeout(1):
+            try:
+                async with async_timeout.timeout(1):
                     message = await channel.get_message(ignore_subscribe_messages=True)
                     if message is not None:
 
-                        
-                        # print(f"(Reader) Message Received: {message}")
-                        # if message["data"] == STOPWORD:
-                        #     print("(Reader) STOP")
-                        #     break
+                        _ = ChatController(_db).add_user(message)
+
                     await asyncio.sleep(0.01)
+
             except asyncio.TimeoutError:
                 pass
 
-    async with psub as p:
+    async with pub_sub as p:
         await p.subscribe("user_created")
         await reader(p)  # wait for reader to complete
         # await p.unsubscribe("channel:1")
--- backend/chat/asgi.py
+++ backend/chat/asgi.py
@@ -9,9 +9,10 @@ import threading
 import uvicorn
 
 from app.main import app
+from app.init_consumer import start
 
 if __name__ == "__main__":
 
-    a = threading.Thread()
+    a = threading.Thread(start, daemon=True)
 
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("OVERSEER_PORT")))
