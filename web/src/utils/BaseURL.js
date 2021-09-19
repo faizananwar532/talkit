211bad25b4efc2d38f5af3f0aa0b349379b04a03
faizananwar532 <faizananwar532@gmail.com>
2021-09-19 10:34:49 +0500

    setting ingress

--- web/src/utils/BaseURL.js
+++ web/src/utils/BaseURL.js
@@ -1,3 +1,3 @@
-export const authBaseUrl = "http://localhost:4000";
-export const chatBaseUrl = "http://localhost:3060";
+export const authBaseUrl = window._env_.AUTH_BASE_URL;
+export const chatBaseUrl = window._env_.CHAT_BASE_URL;
 export const notifBaseUrl = "http://localhost:5000";
