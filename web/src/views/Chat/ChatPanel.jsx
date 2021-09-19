e2c00d5604047fbb1b610b56cdf5669981f4cefa
Awais <awaisshafiq503@gmail.com>
2021-09-19 03:07:15 +0000

    chat panel integration

--- web/src/views/Chat/ChatPanel.jsx
+++ web/src/views/Chat/ChatPanel.jsx
@@ -1,21 +1,80 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useParams } from "react-router";
 import InputTextBox from "../../components/ChatPanel/InputTextBox";
 import MessageBox from "../../components/ChatPanel/MessageBox";
+import { useAuthentication } from "../../context/Authentication";
+import { chatBaseUrl, webSocketUrl } from "../../utils/BaseURL";
+import { openURL } from "../../utils/Utilites";
 
 export default function ChatPanel() {
+
+	const auth = useAuthentication();
+	const params = useParams();
+
+	const [channelMessages, setChannelMessages] = useState([]);
+	const [message, setMessage] = useState();
+
+	const ws = useMemo(() => {
+		if (!params.channel_name) {
+			return;
+		}
+		const ws = new WebSocket(`${webSocketUrl}/ws/${params.channel_name}/${auth.access_token}`);
+		ws.onmessage = function (event) {
+			// var content = document.createTextNode(event.data);
+			console.log(event.data);
+			setChannelMessages([...channelMessages, JSON.parse(event.data)]);
+		};
+		return ws;
+	}, [params.channel_name]);
+
+	
+
+	function sendMessage(event) {
+		event.preventDefault();
+		ws.send(JSON.stringify({ message: message }));
+	}
+
+	const handleInput = (event) => {
+		setMessage(event.target.value);
+	};
+
+	useEffect(async () => {
+
+		const { result, error } = await openURL(chatBaseUrl, `/v1/chat/${params.channel_name}`, "GET", auth);
+
+		if (error) {
+			console.log(error);
+		} else if (result) {
+			setChannelMessages(result.data);
+		}
+
+	}, [params.channel_name]);
+
 	return (
 		<div className="chat-box-container">
 			<div>
-				<InputTextBox/>
+				{
+					params.channel_name && (
+						<form onSubmit={sendMessage}>
+							<InputTextBox onChange={handleInput} />
+						</form>
+					)
+				}
 			</div>
 			<div>
-				<MessageBox sender/>
+				{
+					channelMessages.map((data, index) => {
+						return (
+							<MessageBox sender key={index} message={data.message} />
+						);
+					})
+				}
 				<div className="receiver-message-box">
-					<MessageBox receiver/>
+					<MessageBox receiver />
 				</div>
 			</div>
 			<div>
-				
+
 			</div>
 		</div>
 	);
